2570524dcc98ce6b6d2df90454304d4e837679bc
renelps <renanlps616@gmail.com>
2025-07-14 08:11:21 -0300

    chore: configure main application and Prisma module

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { VehiclesModule } from './vehicles/vehicles.module';
+import { OrdersModule } from './orders/orders.module';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
+import { UsersModule } from './users/users.module';
+import { FavoritesModule } from './favorites/favorites.module';
+import { SimulationsModule } from './simulations/simulations.module';
+import { ReviewsModule } from './reviews/reviews.module';
+import { AuthModule } from './auth/auth.module';
+import { CategoriesModule } from './categories/categories.module';
+import { BrandModule } from './brand/brand.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule,
+    UsersModule,
+    VehiclesModule,
+    OrdersModule,
+    FavoritesModule,
+    SimulationsModule,
+    ReviewsModule,
+    AuthModule,
+    CategoriesModule,
+    BrandModule,
+    CloudinaryModule
+  ],
+})
+export class AppModule {}
---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import 'reflect-metadata';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  await app.listen(3333);
+  console.log('Backend rodando na porta 3333');
+}
+
+void bootstrap();
